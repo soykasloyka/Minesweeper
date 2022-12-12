e5bf78cc5b7c95dd8cb242f28aa08319540bdbb2
Soyka <petr.emelianov@icloud.com>
2022-12-12 08:24:51 +0200

    game space set

--- MinesweeperGame.java
+++ MinesweeperGame.java
@@ -3,5 +3,10 @@ package com.javarush.games.minesweeper;
 import com.javarush.engine.cell.*;
 
 public class MinesweeperGame extends Game {
+    private static final int SIDE = 9;
 
+    @Override
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
+    }
 }
